ec31fe826ae231573692dce3bb705f24792dbd04
justii543 <justinkurian543@gmail.com>
2021-10-24 16:24:07 +0530

    Day1

---
+++ day1.py
@@ -0,0 +1,21 @@
+#single line comment
+
+"""
+multi line comment
+"""
+print("Hello, World")
+
+#numbertype
+a=1j #complex
+print(type(a))
+
+#convertion
+b=1.0
+print(int(b))
+print(complex(b))
+
+#operations
+print(10+3)
+print(2*3)
+print(2**3)   #exponent
+print(2//3)  #floordivision- rounds to nearest floor value
