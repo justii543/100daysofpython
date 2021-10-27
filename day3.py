10768c9dc5dd1ced72b81da4d8c556eb37d84c23
justii543 <justinkurian543@gmail.com>
2021-10-27 08:45:31 +0530

    day3

--- day3.py
+++ day3.py
@@ -3,3 +3,25 @@ print(x.strip())
 print(x.upper())
 print(x.replace("h","y"))
 print(x.split(","))
+
+#concatenation
+a="justin"
+b="kurian"
+print(a+b)
+
+print("just"+"in")
+#conditional
+q=2
+w=2
+if(q>w):
+    print("q is greater")
+elif(q==w):
+    print("eqaul")
+else:
+    print("w is greater") 
+
+#using and ,or, not
+if q>w or q==w:
+    print("set")
+else:
+    print("w is higher")
