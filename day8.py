943f76219ebc5bdd713477d655a2c4df8da2abd7
justii543 <justinkurian543@gmail.com>
2021-11-06 20:02:57 +0530

    day8

--- day8.py
+++ day8.py
@@ -15,3 +15,27 @@ solve(A,B)
 
 # for i in reversed(range(10)):
 #     print(i)
+
+
+#self Keyword - reference of class
+#if you create say two methods in a class and try to call one inside another, 
+# it does not recognize method unless you add "self"--reference of class
+class foo:
+    def sample(self,num1,num2):
+        self.n1=num1
+        self.n2=num2
+    def add(self):
+        return self.n1+self.n2
+
+
+#del keyword
+A=[1,2,3,4,5,6,7,8]
+del A[2]  #o/p--[1,2,4,5,6,7,8]
+
+
+#zip function - returns as tuple
+
+A=["justi","ram","joseph"]
+B=["kurian","cv","mj"]
+p=zip(A,B)
+print(tuple(p))
