516b3e0f236d9131b2007f04602aaa58dc5b2f19
justii543 <justinkurian543@gmail.com>
2021-11-07 23:37:45 +0530

    day10

--- day10.py
+++ day10.py
@@ -1,6 +1,9 @@
 #OBJECTS AND CLASSES
  # __init__ --This method called when an object is created from the class 
  # and it allow the class  to initialize the attributes of a class.
+from typing import List
+
+
 class Human:
     def __init__(self,name ,age):   #self is used to access variables belong to class
          self.name=name
@@ -27,3 +30,37 @@ print(h1.name)
 #empty class
 class Emp:
     pass
+
+
+#lambda function - diff. arguments can be passed but only one expression 
+# can be evaluated.
+x=lambda a,b: a+2*b
+print(x(3,4))
+
+#lambda within another function
+def f1(n):
+    return lambda a:a+n
+x=f1(5)  #5 is passed as argument to f1
+y=f1(8)
+print(x(2))  #2 is passed as argument to lambda funcn within f1
+print(y(4))
+
+#FILTER FUNCTION
+#it filters an array based on boolean array
+
+def prime(x):
+    for i in range(2,x):  
+        if x%2==0:
+            return False
+        else:
+            return True  
+filtered=filter(prime,range(10))  #syntax==> filter(function (which perform test),sequence(list,tuple,..)) 
+                                             #filter function takes the true value only
+print("prime numbers are:  ", list(filtered ))
+
+#MAP FUNCTION
+def square(n):
+    return n*n
+numbers=[1,2,3,4,5]
+mapped=map(square,numbers)  #syntax==> map(function,input values)
+print(list(mapped))
